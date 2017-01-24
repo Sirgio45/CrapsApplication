943cf371f0c2a1532d9cd59e8498f21da1a2eb12
Sirgio45 <15419260@mstc.edu>
2017-01-23 18:56:45 -0600

    Initial commit

---
+++ app/src/main/java/com/ebookfrenzy/crapsapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ebookfrenzy.crapsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startButtonClicked(View view)
+    {
+        Craps.rollDice();
+    }
+
+
+
+
+
+}
