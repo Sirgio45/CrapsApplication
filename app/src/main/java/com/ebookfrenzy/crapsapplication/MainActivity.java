d06d7b9240e6231fd5a881bdc6429e5cb42e6015
Sirgio45 <15419260@mstc.edu>
2017-01-24 09:39:32 -0600

    commit message after the fix.....THanks to WILL

--- app/src/main/java/com/ebookfrenzy/crapsapplication/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/crapsapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -20,16 +21,24 @@ public class MainActivity extends AppCompatActivity {
     private final static int SEVEN=7;
     private final static int YO_ELEVEN=11;
     private final static int BOX_CARS=12;
-    private String playerWinnerMessage;
+
+    private static int bankBalance=1000;
+    private static String displayString="";
+
+
+
 
     TextView playerRolled ;
     TextView playerPointsText ;
     TextView playerWinner ;
     Button rollDice ;
+    TextView messageArray;
 
+    EditText wagerEntered;
+    TextView bankMessage;
 
-
-
+    String [] postiveArray ={"cash those chips in", "High Roller","Do you want to keep pushing your luck?"};
+    String [] negativeArray = {"going for broke?","Did you leave your money at the house?","lets go home now"};
 
 
     @Override
@@ -41,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
         playerPointsText= (TextView)findViewById(R.id.playerPointTextView);
         playerWinner= (TextView)findViewById(R.id.playerWinsTextView);
         rollDice= (Button)findViewById(R.id.startButton);
+        wagerEntered = (EditText)findViewById(R.id.wagerEditText);
+        bankMessage = (TextView)findViewById(R.id.bankTextView);
+        messageArray=(TextView)findViewById(R.id.arrayTextView);
+
+
         rollDice.setOnClickListener(new View.OnClickListener() {
 
 
@@ -49,51 +63,63 @@ public class MainActivity extends AppCompatActivity {
 
 
                 playerRolled.setText("");
+                int wager = Integer.parseInt(wagerEntered.getText().toString());
+                displayString="";
+
+                if (wager<=bankBalance&&wager>0) {
+
+                    int myPoint = 0;
+
+                    Status gameStatus;
+
+                    int sumOfDice = rollDice();
+
+                    switch (sumOfDice) {
+                        case SEVEN:
+                        case YO_ELEVEN:
+                            gameStatus = Status.WON;
+                            //playerWinnerMessage+= "player wins" + myPoint;
+                            break;
+                        case SNAKE_EYES:
+                        case TREY:
+                        case BOX_CARS:
+                            gameStatus = Status.LOST;
+                            break;
+                        default:
+                            gameStatus = Status.CONTINUE;
+                            myPoint = sumOfDice;
+                            playerPointsText.setText(String.format("Point is %d\n", myPoint));
+                            break;
+                    }
+
+                    while (gameStatus == Status.CONTINUE) {
+                        sumOfDice = rollDice();
+
+                        if (sumOfDice == myPoint)
+                            gameStatus = Status.WON;
+                        else if (sumOfDice == SEVEN)
+                            gameStatus = Status.LOST;
 
 
-                int myPoint = 0;
+                    }
 
-                Status gameStatus;
-
-                int sumOfDice = rollDice();
-
-                switch (sumOfDice) {
-                    case SEVEN:
-                    case YO_ELEVEN:
-                        gameStatus = Status.WON;
-                        //playerWinnerMessage+= "player wins" + myPoint;
-                        break;
-                    case SNAKE_EYES:
-                    case TREY:
-                    case BOX_CARS:
-                        gameStatus = Status.LOST;
-                        break;
-                    default:
-                        gameStatus = Status.CONTINUE;
-                        myPoint = sumOfDice;
-                        playerPointsText.setText(String.format("Point is %d\n", myPoint));
-                        break;
-                }
-
-                while (gameStatus == Status.CONTINUE) {
-                    sumOfDice = rollDice();
-
-                    if (sumOfDice == myPoint)
-                        gameStatus = Status.WON;
-                    else if (sumOfDice == SEVEN)
-                        gameStatus = Status.LOST;
-
-
-                }
-
-                if (gameStatus == Status.WON)
-                    playerWinner.setText("Player wins");
-                else
-                    playerWinner.setText("Player loses");
-            }
+                    if (gameStatus == Status.WON) {
+                        playerWinner.setText("Player wins");
+                        displayString+=postiveArray[randomNumbers.nextInt(3)];
+                        messageArray.setText(String.valueOf(displayString));
+                        bankBalance+=wager;
+                        bankMessage.setText("bank is at $"+bankBalance);
+                    }else {
+                        playerWinner.setText("Player loses");
+                        displayString+=negativeArray[randomNumbers.nextInt(3)];
+                        bankBalance-=wager;
+                        messageArray.setText(String.valueOf(displayString));
+                        bankMessage.setText("bank is at $"+bankBalance);
+                    }}}
 
 
         });
+
     }
 
 
